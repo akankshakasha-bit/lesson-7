54f04b47c6ef58229afc0eebb816ebd376d04dba
akankshakasha-bit <akankshakasha@gmail.com>
2025-12-25 20:29:32 +1100

    initial commit

---
+++ activity1.py
@@ -0,0 +1,19 @@
+a = 10
+b = -2
+c = 0
+if a and b and c:
+    print("all numbers are equal to 0")
+else:
+    print("not all numbers are equal to 0")
+
+a=5
+b=-5
+c=0
+if a>0 or b>0:
+    print("either of the numbers are greater than 0")  
+else:
+    print("no number is greater than 0")
+if b>0 or c>0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
